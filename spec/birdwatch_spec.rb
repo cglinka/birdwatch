16a8868dd4ea8d9581e5452dfabc6f6663d2f9b7
Bonnie <blmattson@gmail.com>
2013-09-21 12:00:31 -0500

    add fail test for Chirp::Location#mk_str

--- spec/birdwatch_spec.rb
+++ spec/birdwatch_spec.rb
@@ -1,12 +1,12 @@
 require_relative "../lib/birdwatch.rb"
 
 describe Chirp::Location  do
-  # let (:loc){Chirp::Location.new(-97.75000, 30.25000)}
+  let (:loc){Chirp::Location.new([-97.75000, 30.25000])}
+  let (:locdist){Chirp::Location.new([-97.75000, 30.25000], 10)}
+  
   describe ".initialize" do
     context "When no distance is inputed" do
       it 'sets latitude, longitude, and distance of 5' do
-      
-        loc =Chirp::Location.new(-97.75000, 30.25000)
         expect(loc.lat).to eq(-97.75)
         expect(loc.long).to eq(30.25)
         expect(loc.dist).to eq(5)
@@ -14,12 +14,19 @@ describe Chirp::Location  do
     end
     context "When distance is inputed" do
       it 'sets latitude, longitude and distance' do
-        loc = Chirp::Location.new(-97.75000, 30.25000, 10)
         
-        expect(loc.lat).to eq(-97.75)
-        expect(loc.long).to eq(30.25)
-        expect(loc.dist).to eq(10)
+        expect(locdist.lat).to eq(-97.75)
+        expect(locdist.long).to eq(30.25)
+        expect(locdist.dist).to eq(10)
       end
     end
+  end
+
+  describe "#mk_str" do
+    it "concatenates a string based on initialize" 
+
+      # expect(loc.mk_str).to eq("http://ebird.org/ws1.1/data/obs/geo/recent?lng=-97.75&lat=30.25&dist=5&fmt=json")
+      # expect(locdist.mk_str).to eq("http://ebird.org/ws1.1/data/obs/geo/recent?lng=-97.75&lat=30.25&dist=10&fmt=json")
+    
   end
 end
