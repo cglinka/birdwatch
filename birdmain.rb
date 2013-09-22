39314bf210f022ee94f845c6cd191e7f96a08b5a
Bonnie <blmattson@gmail.com>
2013-09-21 20:47:48 -0500

    fix typo in geo.js, add global js variable birds_json

--- birdmain.rb
+++ birdmain.rb
@@ -13,6 +13,7 @@ get '/birdlist' do
     @lat_lng = cookies[:lat_lng].split("|")
     @loc = Chirp::Location.new(@lat_lng)
     birds_json = @loc.get_list()
+    birds_json = birds_json.to_json
    
     
     erb:birdlist
