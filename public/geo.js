6bd88b078c53e713e40b85db03ec2320b94ef3ba
Bonnie <blmattson@gmail.com>
2013-09-22 12:07:10 -0500

    debug template and js

--- public/geo.js
+++ public/geo.js
@@ -20,8 +20,8 @@ function setGeoCookie(position) {
 
 //Bonnie 
 
-var listTemplateHTML = $("#templates", ".(div class here)");
-var listTemplate = _.template();
+var listTemplateHTML = $('#templates .birdTemplate').html();
+var listTemplate = _.template(listTemplateHTML);
 
 var Birdlist = Backbone.View.extend ({
 	initilize: function (options){
@@ -30,11 +30,36 @@ var Birdlist = Backbone.View.extend ({
 
 	render: function (){
 		for (var i=0; i < this.list.length; i++){
+
 			var newListHTML = listTemplate(this.list[i]);
 			$(this.el).append(newListHTML);
 		}		
 	}
-}
+});
 
-var blist = new Birdlist(window.birds_json)
+//var blist = new Birdlist(window.birds_json)
+
+var testList =[
+    {
+        "comName": "Yellow-crowned Night-Heron",
+        "sciName": "Nyctanassa violacea",
+        "img_src": "http://farm3.staticflickr.com/2453/4034562092_66c80760dc.jpg"
+    },
+    {
+        "comName": "White-winged Dove",
+        "sciName": "Zenaida asiatica",
+        "img_src": "http://farm8.staticflickr.com/7091/7338226784_acb7f13611.jpg"
+    },
+    {
+        "comName": "Rock Pigeon",
+        "sciName": "Columba livia",
+        "img_src": "http://farm4.staticflickr.com/3023/2367694753_cb4a50b22f.jpg"
+    }];
+
+var blist = new Birdlist({
+  list: testList,
+  el: $('.all-search-results')
+});
+blist.render();
+
 
