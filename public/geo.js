e999ed83759983fef6dc43780e077959990ab99e
Clare Glinka <glinka.cb@gmail.com>
2013-09-24 16:11:13 -0500

    Fixed cookie!

--- public/geo.js
+++ public/geo.js
@@ -1,24 +1,17 @@
+// Set cookie with location data on page load.
+navigator.geolocation.getCurrentPosition(setGeoCookie);
+
 function initiate_geolocation(){
-	navigator.geolocation.getCurrentPosition(handle_geolocation_query);
-  // Set cookie with location data
-  navigator.geolocation.getCurrentPosition(setGeoCookie);
   document.location.href = "/birdlist";
 }
 
-function handle_geolocation_query(position){
-	console.log('Lat: ' + position.coords.latitude + ' ' +'Lon: ' + position.coords.longitude);
-	document.getElementById("latitude-text").innerHTML="<h4>latitude:  " + position.coords.latitude +"</h4>";
-	document.getElementById("longitude-text").innerHTML="<h4>longitude:  " + position.coords.longitude +"</h4>";
-}
-
+// Function to set cookie based on HTML5 location data
 function setGeoCookie(position) {
   var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
   document.cookie = "lat_lng=" + escape(cookie_val);
 }
 
+// Click handler for button.
 $('#locate-butt').on('click', function(e){
-  // // navigator.geolocation.getCurrentPosition(setGeoCookie);
-  // alert("cookie set?");
-  // console.log("this button was clicked");
   initiate_geolocation();
 });
